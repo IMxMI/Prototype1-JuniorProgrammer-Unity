704c1a63f5fe0f486039f7acff469e109581fdbf
Maxime Vernoux <mxm.vernoux@gmail.com>
2023-12-12 15:30:04 +0100

    Commentaire script

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -13,6 +13,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Nous allons faire avancer le véhicule.
     }
 }
