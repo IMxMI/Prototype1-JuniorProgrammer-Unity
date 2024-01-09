960a7aabc983eaa7e8a4a9b74fbc6f296a1db413
Maxime Vernoux <mxm.vernoux@gmail.com>
2024-01-09 15:24:05 +0100

    Commentaire code

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,24 +5,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwadInput;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwadInput;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    // Update is called once per frame.
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwadInput = Input.GetAxis("Vertical");
 
-        // Nous allons faire avancer le véhicule.
+        //  Le véhicule avance basé des des entrés verticales.
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwadInput);
+        // Tourne le véhicule basé les des entrés horizontales.
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
